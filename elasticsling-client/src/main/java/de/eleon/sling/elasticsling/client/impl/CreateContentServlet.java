360b41a1b13bae3920ae9dd8826938a0018e32c6
Dominik Förderreuther <dominik@eleon.de>
2016-01-07 00:21:01 +0100

    resolve sample text from node

---
+++ elasticsling-client/src/main/java/de/eleon/sling/elasticsling/client/impl/CreateContentServlet.java
@@ -0,0 +1,43 @@
+package de.eleon.sling.elasticsling.client.impl;
+
+import de.eleon.sling.elasticsling.service.SearchService;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@SlingServlet(paths = {"/create"})
+public class CreateContentServlet extends SlingSafeMethodsServlet {
+
+    @Activate
+    public void activate() {
+        System.out.println(this.getClass().getName() + " STARTED");
+    }
+
+    @Override
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Create Content</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<a href=\"/create?sling:authRequestLogin=1\">auth</a>");
+        out.println("<form method=\"POST\" action=\"/content/\" enctype=\"multipart/form-data\">\n" +
+                "resourceType: <input type=\"text\" name=\"sling:resourceType\" value=\"foo/bar\" /><br />\n" +
+                "title: <input type=\"text\" name=\"title\" value=\"\" /><br />\n" +
+                "text: <input type=\"text\" name=\"text\" value=\"\" /><br />\n" +
+                "<input type=\"submit\" value=\"submit\" />\n" +
+                "</form>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
--- elasticsling-indexer/src/main/java/de/eleon/sling/elasticsling/index/impl/ResourceIndexer.java
+++ elasticsling-indexer/src/main/java/de/eleon/sling/elasticsling/index/impl/ResourceIndexer.java
@@ -2,6 +2,7 @@ package de.eleon.sling.elasticsling.index.impl;
 
 import de.eleon.sling.elasticsling.service.SearchService;
 import org.apache.felix.scr.annotations.*;
+import org.apache.sling.api.resource.*;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.event.Event;
@@ -14,15 +15,29 @@ import java.util.Map;
 
 @Component(immediate=true, metatype=false)
 @Service(EventHandler.class)
-@Property(name = EventConstants.EVENT_TOPIC, value = { SlingConstants.TOPIC_RESOURCE_ADDED, SlingConstants.TOPIC_RESOURCE_CHANGED })
+@Property(name = EventConstants.EVENT_TOPIC, value = { SlingConstants.TOPIC_RESOURCE_ADDED })
 public class ResourceIndexer implements EventHandler {
 
     @Reference
     SearchService searchService;
 
+    @Reference
+    ResourceResolverFactory resolverFactory;
+    ResourceResolver resourceResolver;
+
     @Activate
     public void activate() {
         System.out.println(this.getClass().getName() + " STARTED");
+        try {
+            resourceResolver = resolverFactory.getAdministrativeResourceResolver(null);
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Deactivate
+    public void deactivate() {
+        resourceResolver.close();
     }
 
     @Override
@@ -30,7 +45,16 @@ public class ResourceIndexer implements EventHandler {
         System.out.println("handle event = [" + event + "]");
         String path = get(event, "path", "/index/me");
         String resourceType = get(event, "resourceType", "sling:Folder");
-        searchService.write("/index/me", "my index words");
+        if (resourceType.equalsIgnoreCase("foo/bar")) {
+            index(path);
+        }
+    }
+
+    private void index(String path) {
+        Resource res = resourceResolver.getResource(path);
+        final ValueMap valueMap = res.getValueMap();
+        String text = valueMap.containsKey("text") ? (String)valueMap.get("text") : "";
+        searchService.write(path, text);
     }
 
     private <T> T get(final Event event, String name, T def) {
